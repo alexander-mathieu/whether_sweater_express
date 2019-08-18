d2f037f6f1b883b7baf1445ba87f6ba4b2caa6d7
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-08-17 19:52:59 -0600

    Add initial favorite location list endpoint spec

--- tests/api/v1/favorite_locations.spec.js
+++ tests/api/v1/favorite_locations.spec.js
@@ -23,8 +23,8 @@ describe('favorite locations endpoint', () => {
       return request(app)
       .post('/api/v1/favorites')
       .send({
-        'location': 'Denver, CO',
-        'api_key': user.apiKey
+        location: 'Denver, CO',
+        api_key: user.apiKey
       })
     })
     .then(response => {
@@ -33,12 +33,43 @@ describe('favorite locations endpoint', () => {
     })
   })
 
-  test('returns an error when incorrect API key is sent', () => {
+  test('does not create a new favorite location when incorrect API key is sent', () => {
     return request(app)
     .post('/api/v1/favorites')
     .send({
-      'location': 'Denver, CO',
-      'api_key': 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
+      location: 'Denver, CO',
+      api_key: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
+    })
+    .then(response => {
+      expect(response.statusCode).toBe(401);
+      expect(response.body.error).toEqual('API key is incorrect.');
+    })
+  })
+
+  test('lists favorite locations and current weather for a user when a valid API key is sent', () => {
+    return User.create({
+      email: 'userfavorites2@example.com',
+      password: 'password',
+      apiKey: '11bf5b37-e0b8-42e0-8dcf-dc8c4aefc000'
+    })
+    .then(user => {
+      return request(app)
+      .get('/api/v1/favorites')
+      .send({
+        api_key: user.apiKey
+      })
+    })
+    .then(response => {
+      expect(response.statusCode).toBe(200);
+      expect(response.body.message).toEqual('list');
+    })
+  })
+
+  test('does not return a list of favorite locations and current weather when incorrect API key is sent', () => {
+    return request(app)
+    .get('/api/v1/favorites')
+    .send({
+      api_key: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
     })
     .then(response => {
       expect(response.statusCode).toBe(401);
