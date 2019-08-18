aebc49aaf9af60bee6fd91a742b214e96c6f0aa8
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-08-18 13:37:35 -0600

    Add weather for favorite locations endpoint

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -1,4 +1,7 @@
+require('dotenv').config();
+
 var express = require('express');
+var fetch = require('node-fetch');
 
 var User = require('../../../models').User;
 var Location = require('../../../models').Location;
@@ -31,9 +34,71 @@ router.post('/', (request, response) => {
   })
 })
 
+router.get('/', (request, response) => {
+  return User.findOne({
+    where: {
+      apiKey: request.body.api_key
+    }
+  })
+  .then(user => {
+    if (user) {
+      return user.getLocations()
+      .then(locations => {
+        let allLocationWeather = []
+
+        locations.forEach(location => {
+          let formattedLocation = _formatLocation(location)
+
+          fetch(`https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.GOOGLE_MAPS_API_KEY}&address=${formattedLocation}`)
+          .then(response => {
+            return response.json();
+          })
+          .then(data => {
+            let latLong = data.results[0].geometry.location;
+            let formattedLatLong = _formatLatLong(latLong);
+            return formattedLatLong
+          })
+          .then(formattedLatLong => {
+            return fetch(`https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${formattedLatLong}`)
+          })
+          .then(response => {
+            return response.json();
+          })
+          .then(data => {
+            let locationWeather = {};
+
+            locationWeather.location = formattedLocation;
+            locationWeather.currently = data.currently;
+
+            allLocationWeather.push(locationWeather);
+
+            if (allLocationWeather.length == locations.length) {
+              response.setHeader('Content-Type', 'application/json');
+              response.status(200).send(JSON.stringify({ data: allLocationWeather }));
+            }
+          })
+        })
+      })
+    } else {
+      response.setHeader('Content-Type', 'application/json');
+      response.status(401).send(JSON.stringify({ error: 'API key is incorrect.' }));
+    }
+  })
+})
+
 var _splitLocation = (location) => {
   let cityState = location.split(', ');
   return cityState;
+}
+
+var _formatLocation = (location) => {
+  let combinedLocation =  String(location.city) + ',' + String(location.state);
+  let formattedLocation = combinedLocation.toLowerCase();
+  return formattedLocation
+}
+
+var _formatLatLong = (latLong) => {
+  return (String(latLong.lat) + ',' + String(latLong.lng));
 }
 
 module.exports = router;
